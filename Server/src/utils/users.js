388d4f9289340801a58896d015263dc385f83b90
Elizabeth019 <ponce.elizabeth.j@gmail.com>
2023-04-18 17:04:18 -0300

    Modificado

---
+++ Server/src/utils/users.js
@@ -0,0 +1,9 @@
+// require("dotenv").config();
+// const {PASSWORD} = process.env;
+// //const EMAil = process.env;
+
+
+
+
+
+module.exports = [{username:"lunisj15@gmail.com", password: "romeo2013"}]
